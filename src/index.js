083e4504f77a5201ad398e1820465d43691519c8
SumitG <sghost33@gmail.com>
2017-10-15 17:59:22 +0545

    React Video Player wrapping up

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_details';
+import _ from 'lodash';
+
+
 const API_KEY = 'AIzaSyAucvCKmABpnffsYDa4VUV0laAtA5gb_08';
 
 
@@ -11,19 +14,31 @@ class App extends Component{
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
-    
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-     this.setState({ videos });
-   });
+   this.videoSearch('surfboards');   
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+   });  
   }
   render(){
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
     return(
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos}/>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}/>
       </div>
     );  
   }
